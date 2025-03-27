9d7076a1aef2df6cfc0ac0669c50645a06b41254
Axel <sandilluaxel@gmail.com>
2025-03-27 17:14:10 -0300

    Ejemplos de ejercicios

--- Ejemplos_ejercicios/Ej copy_paste_vector.py
+++ Ejemplos_ejercicios/Ej copy_paste_vector.py
@@ -2,13 +2,14 @@ n=5
 t_vector=[0] * n
 
 numeros=t_vector
+numeros2=t_vector
 
 
 #INICIALIZAR VECTOR/LISTA
 def inicializar_vector(num):
     for i in range(0,n):
         num[i]=0
-        
+
 
 #CARGAR VECTOR/LISTA
 def cargar_vector(num):
@@ -19,12 +20,22 @@ def cargar_vector(num):
 def mostrar_vector(num):
     for i in range(0,n):
         print(f"El elemento que esta en la posicion {i} es: {num[i]}")
+        
+#COPIAR Y PEGAR VECTOR
+def copiar_pegar_vector(num,num2):
+    for i in range(0,n):
+        num2[i]=num[i]
+
 
 
 #BEGIN
 inicializar_vector(numeros)
+inicializar_vector(numeros2)
 cargar_vector(numeros)
+copiar_pegar_vector(numeros,numeros2)
 print()
 mostrar_vector(numeros)
-
+print()
+print("VECTOR COPIADO Y PEGADO: ")
+mostrar_vector(numeros2)
 #END
