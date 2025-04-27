accf8b151cee8601908e2ce96e83abb7bc32b4dd
Axel <sandilluaxel@gmail.com>
2025-04-27 18:14:24 -0300

    Ejercicios practica

--- Ejercicios práctica/Ejercicios del libro/Cola Ejercicios/Ejercicios/punto4.py
+++ Ejercicios práctica/Ejercicios del libro/Cola Ejercicios/Ejercicios/punto4.py
@@ -2,13 +2,35 @@
 from queue import Queue
 from random import randint
 
-number_queue= Queue()
+number_queue = Queue()
 
 def cargarCola(num_queue):
     for i in range(5):
-        num_queue.arrive(randint(1,10))
-        
+        num_queue.arrive(randint(1, 10))
+def es_primo(numero):
+    if numero <= 1:
+        return False
+    for i in range(2, numero):
+        if numero % i == 0:
+            return False
+    return True
+
+def eliminarNoPrimos(num_queue):
+    aux_cola = Queue()
+    
+    while num_queue.size() > 0:
+        element = num_queue.attention()
+        if es_primo(element): # Si es primo, lo agregamos a la cola auxiliar
+            aux_cola.arrive(element)
+    
+    
+    while aux_cola.size() > 0: # Reconstruimos la cola original con los números primos
+        num_queue.arrive(aux_cola.attention())
+
+
 cargarCola(number_queue)
 print("La cola original: ")
 number_queue.show()
-        
+eliminarNoPrimos(number_queue)
+print("La cola después de eliminar los elementos que no son primos:")
+number_queue.show()
