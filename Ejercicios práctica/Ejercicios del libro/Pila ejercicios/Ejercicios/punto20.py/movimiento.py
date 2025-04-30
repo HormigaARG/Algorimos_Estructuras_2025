8aa125a8a14a05afae4efa93c58820bdeb904296
Axel <sandilluaxel@gmail.com>
2025-04-30 14:29:16 -0300

    EP

---
+++ Ejercicios práctica/Ejercicios del libro/Pila ejercicios/Ejercicios/punto20.py/movimiento.py
@@ -0,0 +1,8 @@
+
+class Movimiento:
+    def __init__(self, pasos, direccion):
+        self.pasos = pasos
+        self.direccion = direccion
+
+    def __str__(self):
+        return f"{self.pasos} pasos hacia {self.direccion}"
